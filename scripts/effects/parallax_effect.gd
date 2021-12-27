8ff6d8633e8b8636f9436308acade47a1d357990
davi bandeira <fcodavibandeira@edu.unifor.br>
2021-12-27 20:33:42 -0300

    Animated Effects in Parallax Background(WIP)

---
+++ scripts/effects/parallax_effect.gd
@@ -0,0 +1,29 @@
+extends AnimatedSprite
+
+export(float) var min_wait_time = 1.0
+export(float) var max_wait_time = 5.0
+
+func _ready() -> void:
+	create_timer()
+	
+	
+func create_timer() -> void:
+	var timer: Timer = Timer.new()
+	var _signal = timer.connect("timeout", self, "on_timer_timeout", [timer])
+	add_child(timer)
+	timer.start(get_random_wait_time())
+	
+	
+func get_random_wait_time() -> float:
+	randomize()
+	return rand_range(min_wait_time, max_wait_time)
+	
+	
+func on_timer_timeout(timer: Timer) -> void:
+	timer.queue_free()
+	frame = 0
+	play("effect")
+	
+	
+func on_animation_finished() -> void:
+	create_timer()
